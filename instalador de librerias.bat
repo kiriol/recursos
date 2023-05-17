d2e27e9c5ae2e3919026e6288159678c81cd6115
kiriol <112039097+kiriol@users.noreply.github.com>
2023-05-17 11:31:41 -0400

    Add files via upload
    instalador de librerias para proyecto asistencias SDI

---
+++ instalador de librerias.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Instalando bibliotecas necesarias para el proyecto asistencias del SDI...
+python -m pip install --upgrade pip
+pip install pygame
+pip install opencv-python
+pip install pyzbar
+pip install python-csv
+pip install os-win
+pip install easygui
+echo Bibliotecas instaladas correctamente choro.
+pause
